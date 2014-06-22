7a95d8f6aef8c4945c997d86dfa02a73f25f2054
M <m@i5.(none)>
2014-06-22 16:29:31 -0500

    remove unneeded device makefile

--- device_roamer.mk
+++ device_roamer.mk
@@ -1,4 +1,20 @@
-#DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full.mk)
+
+# Specify phone tech before including full_phone
+$(call inherit-product, vendor/cyanogen/products/gsm.mk)
+
+# Inherit some common CM stuff.
+$(call inherit-product, device/common/gps/gps_us_supl.mk)
+
+#DEVICE_PACKAGE_OVERLAYS += device/zte/roamer/overlay
+
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
+PRODUCT_NAME := aosp_roamer
+PRODUCT_DEVICE := roamer
+PRODUCT_BRAND := ZTE
+PRODUCT_MANUFACTURER := ZTE
+PRODUCT_MODEL := Z990
 
 PRODUCT_AAPT_CONFIG += normal mdpi
 PRODUCT_AAPT_PREF_CONFIG += mdpi
