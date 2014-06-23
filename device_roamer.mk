9194ed59a9492c707fca38cc479333689d4b7961
M <m@i5.(none)>
2014-06-22 19:06:59 -0500

    fix improper product makefile call that copies wrong apn-conf when gsm.mk is removed

--- device_roamer.mk
+++ device_roamer.mk
@@ -1,7 +1,5 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full.mk)
-
-# Inherit some common CM stuff.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 $(call inherit-product, device/common/gps/gps_us_supl.mk)
 
 #DEVICE_PACKAGE_OVERLAYS += device/zte/roamer/overlay
