c0dc44f4e0bf94d3da18549beb583ec61aa967fa
M <m@i5.(none)>
2014-06-30 10:04:16 -0500

    build libril from source instead of copying blob

--- Android.mk
+++ Android.mk
@@ -1,4 +1,4 @@
 
 ifeq ($(TARGET_BOOTLOADER_BOARD_NAME),roamer)
-    include $(call all-named-subdir-makefiles, libaudio libsensors brcm_patchram_plus libril libgralloc libcopybit liblights)
+    include $(call all-named-subdir-makefiles, libril libaudio libsensors brcm_patchram_plus libril libgralloc libcopybit liblights)
 endif
