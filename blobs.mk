faae69a5e579e8ff753f7d5891fb72b711938a5c
M <m@i5.(none)>
2014-08-31 12:37:27 -0500

    add the remaining blobs

--- blobs.mk
+++ blobs.mk
@@ -86,5 +86,53 @@ device/zte/roamer/blobs/pointercal:system/etc/pointercal \
 device/zte/roamer/blobs/com.qualcomm.location.vzw_library.jar:system/framework/com.qualcomm.location.vzw_library.jar \
 device/zte/roamer/blobs/com.qualcomm.location.vzw_library.xml:system/etc/permissions/com.qualcomm.location.vzw_library.xml
 
+# remaining lib blobs
+PRODUCT_COPY_FILES += \
+device/zte/roamer/blobs/libauth.so:system/lib/libauth.so \
+device/zte/roamer/blobs/libcm.so:system/lib/libcm.so \
+device/zte/roamer/blobs/libcommondefs.so:system/lib/libcommondefs.so \
+device/zte/roamer/blobs/libdiag.so:system/lib/libdiag.so \
+device/zte/roamer/blobs/libdll.so:system/lib/libdll.so \
+device/zte/roamer/blobs/libdsi_netctrl.so:system/lib/libdsi_netctrl.so \
+device/zte/roamer/blobs/libdsm.so:system/lib/libdsm.so \
+device/zte/roamer/blobs/libdsprofile.so:system/lib/libdsprofile.so \
+device/zte/roamer/blobs/libdss.so:system/lib/libdss.so \
+device/zte/roamer/blobs/libdsutils.so:system/lib/libdsutils.so
 
+PRODUCT_COPY_FILES += \
+device/zte/roamer/blobs/libgsdi_exp.so:system/lib/libgsdi_exp.so \
+device/zte/roamer/blobs/libgstk_exp.so:system/lib/libgstk_exp.so \
+device/zte/roamer/blobs/libmm-abl.so:system/lib/libmm-abl.so \
+device/zte/roamer/blobs/libmm-adspsvc.so:system/lib/libmm-adspsvc.so \
+device/zte/roamer/blobs/libmmgsdilib.so:system/lib/libmmgsdilib.so \
+device/zte/roamer/blobs/libmsc.so:system/lib/libmsc.so \
+device/zte/roamer/blobs/libnetmgr.so:system/lib/libnetmgr.so \
+device/zte/roamer/blobs/libnfc.so:system/lib/libnfc.so \
+device/zte/roamer/blobs/libnv.so:system/lib/libnv.so
+
+PRODUCT_COPY_FILES += \
+device/zte/roamer/blobs/liboem_rapi.so:system/lib/liboem_rapi.so \
+device/zte/roamer/blobs/libomacurl.so:system/lib/libomacurl.so \
+device/zte/roamer/blobs/liboncrpc.so:system/lib/liboncrpc.so \
+device/zte/roamer/blobs/libpbmlib.so:system/lib/libpbmlib.so \
+device/zte/roamer/blobs/libpdapi.so:system/lib/libpdapi.so \
+device/zte/roamer/blobs/libpdsm_atl.so:system/lib/libpdsm_atl.so \
+device/zte/roamer/blobs/libping_apps.so:system/lib/libping_apps.so \
+device/zte/roamer/blobs/libping_mdm.so:system/lib/libping_mdm.so \
+device/zte/roamer/blobs/libpn544_fw.so:system/lib/libpn544_fw.so \
+device/zte/roamer/blobs/libqcomm_omx.so:system/lib/libqcomm_omx.so \
+device/zte/roamer/blobs/libqmi.so:system/lib/libqmi.so
+
+PRODUCT_COPY_FILES += \
+device/zte/roamer/blobs/libqueue.so:system/lib/libqueue.so \
+device/zte/roamer/blobs/libreference-cdma-sms.so:system/lib/libreference-cdma-sms.so \
+device/zte/roamer/blobs/libsnd.so:system/lib/libsnd.so \
+device/zte/roamer/blobs/libtcmd_jni.so:system/lib/libtcmd_jni.so \
+device/zte/roamer/blobs/libtcmd_pipe.so:system/lib/libtcmd_pipe.so \
+device/zte/roamer/blobs/libuim.so:system/lib/libuim.so \
+device/zte/roamer/blobs/libvdmengine.so:system/lib/libvdmengine.so \
+device/zte/roamer/blobs/libvdmfumo.so:system/lib/libvdmfumo.so \
+device/zte/roamer/blobs/libwms.so:system/lib/libwms.so \
+device/zte/roamer/blobs/libwmsts.so:system/lib/libwmsts.so \
+device/zte/roamer/blobs/libWVphoneAPI.so:system/lib/libWVphoneAPI.so
 
